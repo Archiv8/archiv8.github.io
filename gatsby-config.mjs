1b8a32ef9d21368c70d9b76c117d43b59fb454c7
rossclarkartist <6961242+rossclarkartist@users.noreply.github.com>
2023-08-26 14:38:25 +0100

    :toolbox: CONF: Add plugins and setup plugins
    - Document formatted to comply with repositories current ESLint style guide
    - Added gatsby-plugin-eslint:
      - Currently commented out until configurations understood.
    
    Signed-off-by: rossclarkartist <6961242+rossclarkartist@users.noreply.github.com>
    
    On branch site-source
    Your branch is up to date with 'origin/site-source'.
    - Changes to be committed:
      - modified:   gatsby-config.mjs

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -2,88 +2,135 @@
  * @file Gatsby Site Configuration
  */
 
-import { dirname } from "path"
-import { fileURLToPath } from "url"
-import { createRequire } from "module"
+import { createRequire } from "node:module";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(import.meta.url)
+/*
+ * Const __dirname = path.dirname(fileURLToPath(import.meta.url));
+ * require = createRequire(import.meta.url);
+ * gatsbyEslintRules = path.join(
+ *     process.cwd(),
+ *     "node_modules",
+ *     "gatsby",
+ *     "dist",
+ *     "utils",
+ *     "eslint-rules"
+ * );
+ */
 
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
 const config = {
-  siteMetadata: {
-    title: "Archiv8",
-    siteUrl: "https://archiv8.github.io",
-    description: "Unofficial Arch Linux PKGBUILDs for building packages with tools such as makepkg"
+  "siteMetadata": {
+    "title": "Archiv8",
+    "siteUrl": "https://archiv8.github.io",
+    "description": "Unofficial Arch Linux PKGBUILDs for building packages with tools such as makepkg"
   },
-  trailingSlash: "never",
-  graphqlTypegen: true,
-  plugins: [
+  "trailingSlash": "never",
+  "graphqlTypegen": true,
+  "plugins": [
+
+    /*
+     * {
+     *     "resolve": "gatsby-plugin-eslint",
+     *     "options": {
+     *         // Gatsby required rules directory
+     *         "rulePaths": [gatsbyEslintRules],
+     *         // Default settings that may be omitted or customized
+     *         "stages": ["develop"],
+     *         "extensions": [
+     *             "cjs",
+     *             "js",
+     *             "jsx",
+     *             "mjs",
+     *             "ts",
+     *             "tsx"
+     *         ],
+     *         "exclude": [
+     *             ".cache",
+     *             ".yarn/",
+     *             "__explore/",
+     *             "public",
+     *             "node_modules/",
+     *             "src/images/",
+     *             "src/sounds/",
+     *             "src/videos/",
+     *             ".gitkeep"
+     *         ]
+     */
+
+    //         /*
+    //          * Any additional eslint-webpack-plugin options below
+    //          * ...
+    //          */
+    //     }
+    // },
     "gatsby-plugin-mdx",
     {
-      resolve: "gatsby-source-filesystem",
-      options: {
+      "resolve": "gatsby-source-filesystem",
+      "options": {
         "name": "blogImages",
-        "path": "./src/images/blog-images/",
+        "path": "./src/images/blog-images/"
       },
-      __key: "blogImages"
+      "__key": "blogImages"
     },
     {
-      resolve: "gatsby-source-filesystem",
-      options: {
+      "resolve": "gatsby-source-filesystem",
+      "options": {
         "name": "branding",
-        "path": "./src/images/branding/",
+        "path": "./src/images/branding/"
       },
-      __key: "branding"
+      "__key": "branding"
     },
     {
-      resolve: "gatsby-source-filesystem",
-      options: {
+      "resolve": "gatsby-source-filesystem",
+      "options": {
         "name": "data",
-        "path": "./src/data/",
+        "path": "./src/data/"
       },
-      __key: "data"
+      "__key": "data"
     },
     {
-      resolve: "gatsby-source-filesystem",
-      options: {
+      "resolve": "gatsby-source-filesystem",
+      "options": {
         "name": "icons",
-        "path": "./src/images/icons/",
+        "path": "./src/images/icons/"
       },
-      __key: "icons"
+      "__key": "icons"
     },
     {
-      resolve: "gatsby-source-filesystem",
-      options: {
+      "resolve": "gatsby-source-filesystem",
+      "options": {
         "name": "content",
         "path": "./src/content/"
       },
-      __key: "content"
+      "__key": "content"
     },
     {
-      resolve: "gatsby-source-filesystem",
-      options: {
+      "resolve": "gatsby-source-filesystem",
+      "options": {
         "name": "siteImages",
-        "path": "./src/images/site-images/",
+        "path": "./src/images/site-images/"
       },
-      __key: "siteImages"
+      "__key": "siteImages"
     },
     {
-      resolve: "gatsby-transformer-json",
-      options: {
-        "typeName": "Json",
+      "resolve": "gatsby-transformer-json",
+      "options": {
+        "typeName": "Json"
       }
     },
     {
-      resolve: "gatsby-transformer-yaml",
-      options: {
-        typeName: "Yaml", // a fixed string
-      },
-    },
+      "resolve": "gatsby-transformer-yaml",
+      "options": {
+        // A fixed string
+        "typeName": "Yaml"
+      }
+    }
 
   ]
 };
 
-export default config
+export default config;
